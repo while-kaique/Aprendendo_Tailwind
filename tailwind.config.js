368c3baddd1bd017b6510e3f1696cb04e0388f36
while-kaique <123902283+while-kaique@users.noreply.github.com>
2024-04-28 00:14:02 -0300

    Começando nini-projeto
    Aplicando conhecimentos em Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'selector',
   theme: {
     extend: {
       colors:{
-        'brand': '#000'
+        'brand': '#c5c5c5'
       }
     },
   },
