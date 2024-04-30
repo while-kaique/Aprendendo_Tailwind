1082858fe1ce54457cdc22843d7e3f984df47e82
while-kaique <123902283+while-kaique@users.noreply.github.com>
2024-04-30 14:24:32 -0300

    Introdução criada

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     extend: {
       colors:{
         'brand': '#c5c5c5'
+      },
+      backgroundImage:{
+        'sectionURL': "url('./images/section_img.jpg')",
+        'sectionGradient': 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))',
+        'expandSVG': "URL('./images/icons/expand.svg')",
+        'searchSVG': "URL('./images/icons/search.svg')"
+      },
+      borderWidth:{
+        '10': '20px'
+      },
+      backgroundSize:{
+        'selectionSize': '24px',
+        'searchSize': '30px'
+      },
+      backgroundPosition:{
+        'sectionPosition': 'calc(100% - 8px) center',
+        'searchPosition': '6px center'
       }
+
     },
   },
   plugins: [],
